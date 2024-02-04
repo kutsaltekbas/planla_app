5a524e41d1822c0fc51491fe23d5bdb31deecd99
Kutsal Tekbas <kutsal_tekbass@hotmail.com>
2024-02-04 16:22:01 +0300

    lifting up core app

---
+++ lib/core/constants/app/app_constants.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+class ApplicationConstants {
+  static const TEST_MODE = true;
+  static const LANG_ASSET_PATH = 'assets/languages';
+  static const EMAIL_REGIEX = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}\$';
+  static const LANGUAGE = 'language';
+}
---
+++ lib/core/init/navigation/application_router.dart
@@ -0,0 +1,16 @@
+import 'package:auto_route/auto_route.dart';
+
+// part 'application_router.gr.dart';
+// class AppRouter extends $AppRouter {
+//   @override
+//   List<AutoRoute> getRoutes => [];
+// }
+part 'application_router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        /// routes go here
+      ];
+}
