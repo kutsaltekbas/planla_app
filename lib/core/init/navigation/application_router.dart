70bda0f1b1aa81692e28bc461fbaa8d17e789af3
Kutsal Tekbas <kutsal_tekbass@hotmail.com>
2024-02-15 22:55:12 +0300

    route created

--- lib/core/init/navigation/application_router.dart
+++ lib/core/init/navigation/application_router.dart
@@ -6,6 +6,7 @@ import '../../../modules/plan/main/view/main_view.dart';
 import '../../../modules/plan/profile/view/profile_view.dart';
 import '../../../modules/plan/create_plan/view/create_plan_view.dart';
 import '../../../modules/plan/create_plan/subviews/plan/view/plan_view.dart';
+import '../../../modules/plan/update_plan_navigate_page/view/update_plan_navigator_view.dart';
 
 part 'application_router.gr.dart';
 
@@ -16,6 +17,10 @@ class AppRouter extends _$AppRouter {
         AutoRoute(
           page: SplashRoute.page,
           path: '/',
+        ),
+        AutoRoute(
+          page: UpdatePlanNavigatorRoute.page,
+          path: 'updatePlanNavigator',
         ),
         AutoRoute(
           page: LoginRoute.page,
--- lib/modules/plan/update_plan_navigate_page/view/update_plan_navigator_view.dart
+++ lib/modules/plan/update_plan_navigate_page/view/update_plan_navigator_view.dart
@@ -1,8 +1,10 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/base/view/base_view.dart';
 import '../view_model/update_plan_navigator_view_model.dart';
 
+@RoutePage(name: 'UpdatePlanNavigatorRoute')
 class UpdatePlanNavigatorView extends StatelessWidget {
   const UpdatePlanNavigatorView({Key? key}) : super(key: key);
 
