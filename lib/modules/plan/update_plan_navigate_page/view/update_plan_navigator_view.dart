3479424a391e2bae543514930a96bde7a342f4b3
Kutsal Tekbas <kutsal_tekbass@hotmail.com>
2024-02-15 22:53:31 +0300

    update plan navigator view & view_model added

---
+++ lib/modules/plan/update_plan_navigate_page/view/update_plan_navigator_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/base/view/base_view.dart';
+import '../view_model/update_plan_navigator_view_model.dart';
+
+class UpdatePlanNavigatorView extends StatelessWidget {
+  const UpdatePlanNavigatorView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseView<UpdatePlanNavigatorViewModel>(
+      viewModel: UpdatePlanNavigatorViewModel(),
+      onModelReady: (viewmodel) {
+        viewmodel.setContext(context);
+        viewmodel.init();
+      },
+      onPageBuilder: (context, viewmodel) {
+        return Scaffold();
+      },
+    );
+  }
+}
